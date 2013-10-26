d9264be6abfd963c06bf2b702c53f2576874444c
Bernat Borrás Paronella <bernatbor15@gmail.com>
2013-10-27 00:52:12 +0200

    Gifts download from Server

--- WedList/src/main/java/es/catmobil/wedlist/syncadapter/SyncAdapter.java
+++ WedList/src/main/java/es/catmobil/wedlist/syncadapter/SyncAdapter.java
@@ -7,6 +7,7 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.SyncResult;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -22,7 +23,9 @@ import java.util.List;
 
 import es.catmobil.wedlist.application.MyConstants;
 import es.catmobil.wedlist.database.contract.DataContract;
+import es.catmobil.wedlist.database.cursor.GiftCursor;
 import es.catmobil.wedlist.database.cursor.ProjectCursor;
+import es.catmobil.wedlist.model.Gift;
 import es.catmobil.wedlist.model.Project;
 
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
@@ -33,66 +36,143 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-        Log.v("PARSE", "Performing sync");
-        {//ALL Projects from a user
+        downloadProjects(account);
+        downloadGifts();
+    }
 
-            ParseQuery<ParseObject> query = ParseQuery.getQuery("Invitation");
+    private void downloadProjects(Account account) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Invitations");
+        query.whereEqualTo(MyConstants.PARSE_USER, "user1@gmail.com");
+        query.findInBackground(new ProjectsCallback());
+    }
 
-            // Restrict to cases where the author is the current user.
-            query.whereEqualTo(MyConstants.PARSE_USER, "user1@gmail.com");
+    private class ProjectsCallback extends FindCallback<ParseObject> {
 
-            // Run the query
+        @Override
+        public void done(List<ParseObject> invitations,
+                         ParseException e) {
+            Log.v("PARSE", "Performing sync done ");
+            if (e == null) {
+                // If there are results, update the list of posts
+                // and notify the adapter
+                // result.clear();
+                final List<Project> projects = new ArrayList<Project>(invitations.size());
+                for (ParseObject po : invitations) {
+                    String id = po.getString("project");
+                    Log.d("PARSE", "Invitation projectid:" + id);
+                    ParseQuery<ParseObject> queryProject = ParseQuery.getQuery("Project");
+                    queryProject.whereEqualTo("objectId", id);
+                    queryProject.getFirstInBackground(new GetProjectCallback(projects));
+                }
+            } else {
+                Log.d("PARSE", "Performing sync Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private class GetProjectCallback extends GetCallback<ParseObject> {
+
+        private final List<Project> projects;
+
+        public GetProjectCallback(List<Project> projects) {
+            this.projects = projects;
+        }
+
+        @Override
+        public void done(ParseObject object, ParseException e) {
+            ContentResolver contentResolver = getContext().getContentResolver();
+            contentResolver.delete(DataContract.ProjectTable.CONTENT_URI, null, null);
+            if (object == null) {
+                Log.d("PARSE", "The getFirst request failed.");
+            } else {
+                Project project = new Project();
+                project.setName(object.getString("name"));
+                project.setImage(object.getString("image"));
+                project.setDescription(object.getString("description"));
+                //project.setDate(object.getDate("date"));
+                project.setServerId(object.getObjectId());
+                project.setExtras(object.getString("extras"));
+
+                projects.add(project);
+
+                Log.i("WEB-TAG", project.toString());
+            }
 
 
-            query.findInBackground(new FindCallback<ParseObject>() {//List of invitations
 
-                @Override
-                public void done(List<ParseObject> invitations,
-                                 ParseException e) {
-                    Log.v("PARSE", "Performing sync done ");
-                    if (e == null) {
-                        // If there are results, update the list of posts
-                        // and notify the adapter
-                        // result.clear();
-                        final List<Project> projects = new ArrayList<Project>(invitations.size());
-                        for (ParseObject po : invitations) {
-                            //result.add(po.getString(DataContract.ProjectColumns.NAME));
-                            String id = po.getString("project");
-                            Log.d("PARSE", "Invitation projectid:" + id);
-                            ParseQuery<ParseObject> queryProject = ParseQuery.getQuery("Project");
-                            queryProject.whereEqualTo("objectId", id);
-                            queryProject.getFirstInBackground(new GetCallback<ParseObject>() {
-                                public void done(ParseObject object, ParseException e) {
-                                    if (object == null) {
-                                        Log.d("PARSE", "The getFirst request failed.");
-                                    } else {
-                                        Project project = new Project();
-                                        project.setName(object.getString("name"));
-                                        project.setImage(object.getString("image"));
+            ContentValues[] projectsValues = new ProjectCursor(getContext()).setValuesArray(projects);
+            contentResolver.bulkInsert(DataContract.ProjectTable.CONTENT_URI, projectsValues);
+        }
+    }
 
-                                        projects.add(project);
-                                        Log.v("PARSE-TAG", "project :" + project.getName());
-                                        Log.v("PARSE-TAG", "project :" + project.getImage());
-                                    }
+    private void downloadGifts() {
+        Cursor projectsCursor = getContext().getContentResolver().query(DataContract.ProjectTable.CONTENT_URI, null, null, null, null);
 
-                                    ContentResolver contentResolver = getContext().getContentResolver();
+        if (projectsCursor != null) {
+            ContentResolver contentResolver = getContext().getContentResolver();
+            contentResolver.delete(DataContract.GiftTable.CONTENT_URI, null, null);
+            while (projectsCursor.moveToNext()){
+                String serverId = projectsCursor.getString(projectsCursor.getColumnIndex(DataContract.ProjectTable.ProjectColumns.SERVER_ID));
+                Long id = projectsCursor.getLong(projectsCursor.getColumnIndex(DataContract.ProjectTable.ProjectColumns._ID));
 
-                                    ContentValues[] projectsValues = new ProjectCursor(getContext()).setValuesArray(projects);
+                Log.i("PARSE-TAG", "Project server id: " + serverId);
 
-                                    contentResolver.delete(DataContract.ProjectTable.CONTENT_URI, null, null);
-                                    contentResolver.delete(DataContract.GiftTable.CONTENT_URI, null, null);
-                                    contentResolver.bulkInsert(DataContract.ProjectTable.CONTENT_URI, projectsValues);
-                                }
-                            });
+                if (serverId != null) {
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery("Gifts");
+                    query.whereEqualTo(MyConstants.PARSE_PROJECT_ID, serverId);
+                    query.findInBackground(new GiftsCallback(id));
+                }
+            }
+        }
+    }
 
-                        }
-                    } else {
-                        Log.d("PARSE", "Performing sync Error: " + e.getMessage());
-                    }
+    private class GiftsCallback extends FindCallback<ParseObject> {
+
+        private Long id;
+
+        public GiftsCallback(Long id) {
+
+            this.id = id;
+        }
+
+        @Override
+        public void done(List<ParseObject> gifts, ParseException e) {
+            if (e == null) {
+                // If there are results, update the list of posts
+                // and notify the adapter
+                // result.clear();
+                final List<Gift> giftList = new ArrayList<Gift>(gifts.size());
+                for (ParseObject po : gifts) {
+                    String serverId = po.getString("objectId");
+
+                    Gift gift = new Gift();
+
+                    gift.setServerId(serverId);
+                    gift.setName(po.getString("name"));
+                    gift.setDescription(po.getString("description"));
+                    gift.setPicturePath(po.getString("picture_url"));
+                    gift.setPrice(Float.parseFloat(po.getString("price")));
+                    gift.setBought(po.getBoolean("bought"));
+
+                    giftList.add(gift);
                 }
 
-            });
+                Log.i("PARSE-TAG", "Gifts at project: " + giftList.size());
 
+                ContentResolver cr = getContext().getContentResolver();
+
+                ContentValues[] values = new GiftCursor(getContext()).setValuesArray(giftList);
+
+                for (ContentValues v:values) {
+                    v.put(DataContract.GiftTable.GiftColumns.PROJECT, id);
+                }
+
+                cr.bulkInsert(DataContract.GiftTable.CONTENT_URI, values);
+
+
+            } else {
+                Log.d("PARSE", "Performing sync Error: " + e.getMessage());
+            }
         }
     }
 }
