89552a6713a9ff3fe23d36ff8e62622c634626c1
Bernat Borrás Paronella <bernatbor15@gmail.com>
2013-10-26 14:49:44 +0200

    Paypal payments and sync adapters ready

---
+++ WedList/src/main/java/es/catmobil/wedlist/syncadapter/SyncAdapter.java
@@ -0,0 +1,23 @@
+package es.catmobil.wedlist.syncadapter;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+/**
+ * Created by Bernat on 26/10/13.
+ */
+public class SyncAdapter extends AbstractThreadedSyncAdapter{
+
+    public SyncAdapter(Context context) {
+        super(context, true);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+
+    }
+}
