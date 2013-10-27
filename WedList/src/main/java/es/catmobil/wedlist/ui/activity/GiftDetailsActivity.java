f1efd0b2ee8350fcde79beff7a6e0ac1444e1e52
adria <adriaberge@gmail.com>
2013-10-27 02:37:13 +0200

    tot va2

--- WedList/src/main/java/es/catmobil/wedlist/ui/activity/GiftDetailsActivity.java
+++ WedList/src/main/java/es/catmobil/wedlist/ui/activity/GiftDetailsActivity.java
@@ -1,6 +1,7 @@
 package es.catmobil.wedlist.ui.activity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -8,6 +9,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 
 import es.catmobil.wedlist.R;
+import es.catmobil.wedlist.database.contract.DataContract;
+import es.catmobil.wedlist.ui.fragment.ComplexGiftDetailFragment;
 import es.catmobil.wedlist.ui.fragment.GiftsListFragment;
 import es.catmobil.wedlist.ui.fragment.SimpleGiftDetailFragment;
 import es.catmobil.wedlist.ui.fragment.WedsDetailsFragment;
@@ -28,8 +31,19 @@ public class GiftDetailsActivity extends ActionBarActivity{
     @Override
     protected void onResume() {
         super.onResume();
+        Fragment f = null;
+        String where = DataContract.GiftTable.GiftColumns._ID+" = "+id;
+        Cursor cursor = getContentResolver().query(DataContract.GiftTable.CONTENT_URI,null,where,null,null);
+        cursor.moveToFirst();
+        String complex = "";//cursor.getString(cursor.getColumnIndex(DataContract.GiftTable.GiftColumns.COMPLEX));
+
+        if(Boolean.parseBoolean(complex) || cursor.getCount() == 0){
+            f = ComplexGiftDetailFragment.newInstance(id);
+        }else{
+            f = SimpleGiftDetailFragment.newInstance(id);
+        }
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.content1, SimpleGiftDetailFragment.newInstance(id));
+        ft.replace(R.id.content1, f);
         ft.commit();
     }
 
---
+++ WedList/src/main/java/es/catmobil/wedlist/ui/fragment/ComplexGiftDetailFragment.java
@@ -0,0 +1,35 @@
+package es.catmobil.wedlist.ui.fragment;
+
+import android.database.Cursor;
+import android.os.Bundle;
+
+import es.catmobil.wedlist.R;
+import es.catmobil.wedlist.database.contract.DataContract;
+
+/**
+ * Created by adria on 27/10/13.
+ */
+public class ComplexGiftDetailFragment extends BaseGiftDetailFragment{
+    private static final String param = "id";
+
+    public static ComplexGiftDetailFragment newInstance(int id) {
+        ComplexGiftDetailFragment fragment = new ComplexGiftDetailFragment();
+        Bundle arg = new Bundle();
+        arg.putInt(param,id);
+        fragment.setArguments(arg);
+        return fragment;
+    }
+    @Override
+    public int getResourcesLayout() {
+        return R.layout.fragment_gift_detail_complex;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String where = DataContract.GiftTable.GiftColumns._ID+" = "+getArguments().getInt(param);
+
+        Cursor cursor = getActivity().getContentResolver().query(DataContract.GiftTable.CONTENT_URI,null,where,null,null);
+        this.putDateIntoItsContainer(cursor);
+    }
+}
+
