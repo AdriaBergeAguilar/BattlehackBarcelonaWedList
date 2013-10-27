0b0c9d2f06fd8c5397e7bf720d5c3d362022418d
mikelcoll <mikelcoll@MBPR-Miquel.local>
2013-10-27 05:18:54 +0100

    merge

--- WedList/src/main/java/es/catmobil/wedlist/ui/activity/GiftDetailsActivity.java
+++ WedList/src/main/java/es/catmobil/wedlist/ui/activity/GiftDetailsActivity.java
@@ -57,19 +57,13 @@ public class GiftDetailsActivity extends ActionBarActivity implements GiftsListF
 
         if (cursor != null && cursor.moveToFirst()) {
             Gift g = new GiftCursor(this).readValues(cursor);
-<<<<<<< HEAD
+
             Cursor cursor2 = getContentResolver().query(DataContract.ProjectTable.CONTENT_URI, null, DataContract.ProjectTable.ProjectColumns._ID + "=" + g.getProjectId(), null, null);
             cursor2.moveToFirst();
             ProjectCursor proc = new ProjectCursor(this);
             Project proj =proc.readValues(cursor2);
             email_receptor = proj.getEmail();
 
-=======
-            Cursor cursor2 = getContentResolver().query(DataContract.ProjectTable.CONTENT_URI, null, DataContract.PersonTable.PersonColumns._ID + "=" + g.getProjectId(), null, null);
-            if (cursor2 != null && cursor2.moveToFirst()) {
-                email_receptor = cursor2.getString(cursor2.getColumnIndex(DataContract.ProjectTable.ProjectColumns.EMAIL));
-            }
->>>>>>> 2ddbf4deeb4d1945d8e9760a1820c1f9c58e8afe
             boolean complex = g.isComplex();
             if (complex) {
                 f = ComplexGiftDetailFragment.newInstance(id);
